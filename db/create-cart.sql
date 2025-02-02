9de4072303d7ac097739d15d5d7dc113206f5a99
Kelly Hu <104221443+huiskellyhu@users.noreply.github.com>
2025-02-02 02:51:58 -0800

    Update create-cart.sql

--- db/create-cart.sql
+++ db/create-cart.sql
@@ -1,3 +1,7 @@
+ALTER TABLE movies
+MODIFY COLUMN id varchar(10)
+COLLATE utf8mb4_0900_ai_ci;
+
 CREATE TABLE cart_items (
     id INT AUTO_INCREMENT PRIMARY KEY,
     customer_id INT NOT NULL,
@@ -6,3 +10,4 @@ CREATE TABLE cart_items (
     FOREIGN KEY (customer_id) REFERENCES customers(id),
     FOREIGN KEY (movie_id) REFERENCES movies(id)
 );
+
