a4b5cc7d7ce7bdad112605a5244d6c653fe17358
huiskellyhu <kellyhu96@gmail.com>
2025-02-28 08:55:30 -0800

    fix project5 t4 routing writes

--- src/AddToCartServlet.java
+++ src/AddToCartServlet.java
@@ -26,7 +26,7 @@ public class AddToCartServlet extends HttpServlet {
 
     public void init(ServletConfig config) {
         try {
-            dataSource = (DataSource) new InitialContext().lookup("java:comp/env/jdbc/moviedb");
+            dataSource = (DataSource) new InitialContext().lookup("java:comp/env/master");
         } catch (NamingException e) {
             e.printStackTrace();
         }
